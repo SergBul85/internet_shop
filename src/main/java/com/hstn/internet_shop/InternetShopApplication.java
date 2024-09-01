c7a2db64215eed7f1ebe2ce38b9455f8e5e19589
Serhii <donetsk55555@gmail.com>
2024-09-01 23:09:52 +0300

    Init commit

---
+++ src/main/java/com/hstn/internet_shop/InternetShopApplication.java
@@ -0,0 +1,13 @@
+package com.hstn.internet_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hstn/internet_shop/InternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hstn.internet_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
